d739715de93349aeef2416a0ffc09885b02dcef9
leo palmer sunmo <leo.sunmo@gmail.com>
2019-11-20 10:55:46 +1300

    Add overwrite file option

--- main.go
+++ main.go
@@ -28,19 +28,24 @@ func readV0VSYaml(file string) (glooV0.VirtualService, error) {
 	return vs, nil
 }
 
-func writeV1Gloo(sourceFile string, v1VirtualService glooV1.VirtualService) error {
+func writeV1Gloo(sourceFile string, v1VirtualService glooV1.VirtualService, inplace bool) error {
 	var err error
 	var fileName string
 	if l := strings.LastIndex(sourceFile, ".yaml"); l != -1 {
-		if strings.Contains(sourceFile, "-vs.yaml") {
-			fileName = strings.Replace(sourceFile, "-vs.yaml", "-vs2.yaml", 1)
+		if !inplace {
+			if strings.Contains(sourceFile, "-vs.yaml") {
+				fileName = strings.Replace(sourceFile, "-vs.yaml", "-vs2.yaml", 1)
+			} else {
+				fileName = sourceFile[:l] + "-vs2" + sourceFile[l:]
+			}
 		} else {
-			fileName = sourceFile[:l] + "-vs2" + sourceFile[l:]
+			fileName = sourceFile
 		}
 	} else {
 		fileName = sourceFile + ".yaml"
 	}
-	f, fErr := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0664)
+
+	f, fErr := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0664)
 	if fErr != nil {
 		return err
 	}
@@ -59,13 +64,14 @@ func writeV1Gloo(sourceFile string, v1VirtualService glooV1.VirtualService) erro
 func main() {
 
 	kSvc := flag.Bool("k", false, "Convert Upstreams to KubeSvc routes")
+	inplace := flag.Bool("i", false, "Overwrite input file in place")
 	flag.Parse()
 	var files []string
 	if len(flag.Args()) > 0 {
 		files = flag.Args()
 	} else {
 		fmt.Println(len(flag.Args()))
-		fmt.Printf("Usage: %s [-k] <one or more YAML files>\n", os.Args[0])
+		fmt.Printf("Usage: %s [-k] [-i] <one or more YAML files>\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -83,7 +89,7 @@ func main() {
 			log.Printf("err while processing %s, %s\n", file, err.Error())
 			continue
 		}
-		fErr := writeV1Gloo(file, v1Vs)
+		fErr := writeV1Gloo(file, v1Vs, *inplace)
 		if fErr != nil {
 			log.Printf("failed to write %s, %s\n", file, fErr.Error())
 		}
